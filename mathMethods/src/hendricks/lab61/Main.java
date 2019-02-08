1a8f658d267d80463c896af02cd36d0e7614bc49
Connor <cam-o-man@live.com>
2019-02-08 14:36:19 -0500

    Issues with mathMethods, working to resolve.
    Signed-off-by: Connor <cam-o-man@live.com>

--- mathMethods/src/hendricks/lab61/Main.java
+++ mathMethods/src/hendricks/lab61/Main.java
@@ -12,10 +12,15 @@ public class Main {
         int agex = input.nextInt();
         int agey = input.nextInt();
         int diff = Math.abs(agey - agex);
-        System.out.println("A chart of numbers and their roots.");
         System.out.println(diff);
+        System.out.println("A chart of numbers and their roots.");
         for (int i = 0; i < 20; i++) {
-            System.out.println(i + "\t\t" + Math.sqrt(i));
+            if (Math.sqrt(i) % 1 == 0) {
+                System.out.println(i + "\t\t" + Math.sqrt(i));
+            }
+            else {
+                System.out.println(i + "\t\t" + "\u221A" + i);
+            }
         }
         System.out.println("A chart of numbers and their powers.");
         for (int i = 1; i < 6; i++) {
@@ -25,15 +30,46 @@ public class Main {
         int int1 = input.nextInt();
         int int2 = input.nextInt();
         System.out.println(Math.max(int1, int2));
+        halloween();
+        bacteria();
+        pythag();
     }
     private static void halloween() {
         Scanner input = new Scanner(System.in);
         System.out.println("Hello, students!");
         System.out.println("Input your height.");
         int height = input.nextInt();
-        double Area = 2 * (Math.PI * (Math.pow((height / 2.0d), 2)));
+        double Area = (Math.PI * (Math.pow((height / 2.0d), 2)));
         long roundedArea = Math.round(Area);
+        System.out.println("You'll need:");
         System.out.println(roundedArea);
+        System.out.println("square inches of fabric, and:");
+        double circum = 2 * (Math.PI * height);
+        System.out.println(Math.round(circum));
+        System.out.println("inches of blood tubing.");
+    }
+    private static void bacteria() {
+        System.out.println("If at time t = 0, there are 40 bacteria, then after 24 hours there will be:");
+        double bac;
+        int initbac = 40;
+        int t = 24;
+        double k = 0.25935;
 
+        //--------------------------------------------------------
+
+        bac = initbac * Math.pow(Math.E, (k * t));
+
+        int roundedBac = (int)Math.round(bac);
+        System.out.println(roundedBac);
+        System.out.println("bacteria.");
+
+
+    }
+    private static void pythag() {
+        double rightLeg = 9.0d;
+        double leftLeg = 13.0d;
+        double hypotenuse = (int)Math.round(Math.hypot(rightLeg, leftLeg)); //Finding hypotenuse
+        int angle = (int)Math.round(Math.sin(rightLeg/hypotenuse));
+        System.out.println("The angle is " + angle + "\u00b0");
     }
 }
