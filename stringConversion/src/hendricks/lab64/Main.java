4bdd4aebf7d072b54bf21a450501defe82f77d5e
Connor <cam-o-man@live.com>
2019-02-22 20:50:34 -0500

    Finished 6.4

--- stringConversion/src/hendricks/lab64/Main.java
+++ stringConversion/src/hendricks/lab64/Main.java
@@ -1,5 +1,7 @@
 package hendricks.lab64;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,11 +12,36 @@ public class Main {
     }
     public static void wordy() {
         String circumlocutory = "AB2CD8EF9G";
-        int[] numsInString;
+        StringBuilder numstring = new StringBuilder(); //More efficient than an ArrayList, as I'd just have to iterate through the ArrayList.
+        int count = 0;
         for (Character object: circumlocutory.toCharArray()) {
             if (Character.isDigit(object)) {
-                //TODO: yeet the digits into numsInString and then make it an int
+                numstring.append(object);
             }
         }
+        int number = Integer.parseInt(numstring.toString());
+        System.out.println(number);
+        int sum = 0;
+        while (number > 0) {
+            sum = sum + (number % 10);
+            number = number / 10;
+        }
+        System.out.println(sum);
+        launch();
+    }
+    public static void launch() {
+        String Launch = "54321";
+        StringBuilder go = new StringBuilder();
+        int sum = 0;
+        for (Character object: Launch.toCharArray()) {
+            go.append(object);
+            go.append(" - ");
+            sum += Integer.parseInt(Character.toString(object));
+
+        }
+        go.replace(16, 18, "GO!");
+        System.out.println(go);
+        System.out.println(sum + " - GO!");
+        //TODO: Test program and debug
     }
 }
