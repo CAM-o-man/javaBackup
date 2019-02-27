2d3ba436613a9dadd68ecd4f5fff564688b6382b
Connor <cam-o-man@live.com>
2019-02-27 09:18:07 -0500

    Began new lab.

---
+++ createFiles/src/hendricks/lab71/Main.java
@@ -0,0 +1,12 @@
+package hendricks.lab71;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        File create = new File("create.txt"); //Should use a relative path and put it in createFiles/src/hendricks/lab71/create.txt
+        FileOutputStream outputStream = new FileOutputStream()
+    }
+}
