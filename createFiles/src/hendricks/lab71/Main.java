4f3c8e1b5c904e614fde0d438447389fe2414a3c
Connor <cam-o-man@live.com>
2019-03-01 11:44:26 -0500

    Did more work on 7.1

--- createFiles/src/hendricks/lab71/Main.java
+++ createFiles/src/hendricks/lab71/Main.java
@@ -2,11 +2,28 @@ package hendricks.lab71;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         File create = new File("create.txt"); //Should use a relative path and put it in createFiles/src/hendricks/lab71/create.txt
-        FileOutputStream outputStream = new FileOutputStream()
+        FileOutputStream outputStream = new FileOutputStream(create);
+        String name = "Connor McDermid";
+        String course = "Java";
+        String grades = "B, B, B";
+        String teacher = "Hendricks";
+        byte[] nameBytes = name.getBytes();
+        byte[] courseBytes = course.getBytes();
+        byte[] gradeBytes = grades.getBytes();
+        byte[] teacherBytes = teacher.getBytes();
+        outputStream.write(nameBytes);
+        outputStream.write(courseBytes);
+        outputStream.write(gradeBytes);
+        outputStream.write(teacherBytes);
+    }
+    public static void payroll() throws IOException{
+        File create = new File("payroll.txt");
+        
     }
 }
