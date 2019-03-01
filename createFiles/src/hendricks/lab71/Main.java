6070d72685322f538559fb742a218aed46af1eda
Connor <cam-o-man@live.com>
2019-03-01 14:07:46 -0500

    halfway done last method, just saving work

--- createFiles/src/hendricks/lab71/Main.java
+++ createFiles/src/hendricks/lab71/Main.java
@@ -2,6 +2,7 @@ package hendricks.lab71;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -21,9 +22,33 @@ public class Main {
         outputStream.write(courseBytes);
         outputStream.write(gradeBytes);
         outputStream.write(teacherBytes);
+        payroll();
     }
     public static void payroll() throws IOException{
-        File create = new File("payroll.txt");
+        Scanner input = new Scanner(System.in);
+        File payroll = new File("payroll.txt");
+        FileOutputStream outputStream = new FileOutputStream(payroll);
+        String userinput;
+        byte[] userBytes;
+        System.out.println("Please enter employee information and pay on one line.");
+        do {
+           userinput = input.nextLine();
+           if (userinput.equals(":wq")) {
+               break;
+           } else {
+               userBytes = userinput.getBytes();
+               outputStream.write(userBytes);
+           }
+        } while (true);
+    }
+    public static void numbers() {
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+        File numbers = new File("numbers.txt");
+        FileOutputStream outputStream = new FileOutputStream(numbers);
+        System.out.println("Please input the amout of random numbers to be generated.");
+        int amount = input.nextInt();
         
     }
+
 }
