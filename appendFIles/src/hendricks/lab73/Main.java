d0bec0f3f0c5e03f3676a51ed13ef5cb02337f82
Connor <cam-o-man@live.com>
2019-03-15 08:19:28 -0400

    Finished 7.3 and 6.8 test files
    Signed-off-by: Connor <cam-o-man@live.com>

--- appendFIles/src/hendricks/lab73/Main.java
+++ appendFIles/src/hendricks/lab73/Main.java
@@ -1,30 +1,55 @@
 package hendricks.lab73;
+import org.jetbrains.annotations.NotNull;
+
 import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
         File dog = new File("dog.txt");
-        FileOutputStream outputStream = new FileOutputStream(dog);
         FileOutputStream appendStream = new FileOutputStream(dog, true);
-        BufferedReader reader = new BufferedReader(new FileReader(dog));
-
+        readFile(dog);
+        //_____________________________________________________________________________________________________
+        //Add dogs to file
+        String dog1 = "\nBeatrice, 3";
+        String dog2 = "\nTasha 4";
+        String dog3 = "\nPatty 1";
+        appendStream.write(dog1.getBytes());
+        appendStream.write(dog2.getBytes());
+        appendStream.write(dog3.getBytes());
+        System.out.println("File updated. Reading.");
+        readFile(dog);
     }
-    public static int parser(String nameandage) {
-        String age = nameandage.substring(nameandage.indexOf(' '));
+
+    private static void readFile(File dog) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(dog));
+        boolean dogs;
+        String line;
+        int age;
+        String name;
+        do {
+            line = reader.readLine();
+            if (line != null) {
+                dogs = true;
+                age = parser(line);
+                name = nameGet(line);
+                if (age > 7) {
+                    System.out.println(name + " OLD DOG");
+                } else {
+                    System.out.println(name + " YOUNG DOG");
+                }
+            } else {
+                dogs = false;
+            }
+        } while(dogs);
+    }
+
+    public static int parser(@NotNull String s) {
+        String age = s.substring(s.indexOf(' ') + 1);
         int realage = Integer.parseInt(age);
         return realage;
     }
-    public static String nameGet(String s) {
-
-    }
-    public static String[] dogs(String name, int age) {
-        String[] dog = new String[2];
-        dog[0] = name;
-        if (age < 7) {
-            dog[1] = "YOUNG DOG";
-        } else {
-            dog[1] = "OLD DOG";
-        }
-        return dog;
+    public static String nameGet(@NotNull String s) {
+        String name = s.substring(0, s.indexOf(' '));
+        return name;
     }
 }
